334a6cdacad4b0a382f172213b7d62ffd38c9ae5
Evgeniy <devil_98@abv.bg>
2022-05-05 09:20:43 +0300

    js nested loops

---
+++ Basics/Nested Loops/clock.js
@@ -0,0 +1,16 @@
+// function clock(){
+//     for (let x = 0; x <= 23; x++){
+//         for (let y = 0; y <= 59; y++){
+//             console.log(`${x}:${y}`)
+//         }
+//     }
+// }
+
+// clock();
+
+
+for (let x = 0; x <= 23; x++){
+    for (let y = 0; y <= 59; y++){
+        console.log(`${x}:${y}`)
+    }
+}
