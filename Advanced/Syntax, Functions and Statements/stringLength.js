aef508bdc3e776ff2f9d3d541869da1053bf75f9
Evgeniy <devil_98@abv.bg>
2022-05-17 22:22:17 +0300

    string len

---
+++ Advanced/Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,7 @@
+function stringLength(arr1, arr2, arr3){
+    let len = arr1.length + arr2.length + arr3.length;
+    console.log(len)
+    console.log(Math.floor(len / 3))
+};
+
+stringLength('chocolate', 'ice cream', 'cake');
