b656016729cefb23054dcb5209fc3f0c3c321f39
Evgeniy <devil_98@abv.bg>
2022-05-23 22:21:29 +0300

    fixed

--- Advanced/Arrays and Nested Arrays/biggestElement.js
+++ Advanced/Arrays and Nested Arrays/biggestElement.js
@@ -1,18 +1,20 @@
 function biggestElement(matrix){
-    let biggestNumber = Number.MIN_VALUE;
-    for (row in matrix){
-        for (col in matrix[row]){
-            let number = matrix[row][col];
-            if (number > biggestNumber){
-                biggestNumber = matrix[row][col];
-            };
-        };
-    };
-    console.log(biggestNumber);;
+    // let biggestNumber = Number.MIN_VALUE;
+    // for (row in matrix){
+    //     for (col in matrix[row]){
+    //         let number = matrix[row][col];
+    //         if (number > biggestNumber){
+    //             biggestNumber = matrix[row][col];
+    //         };
+    //     };
+    // };
+    // return biggestNumber;
+    const max = Math.max(...[].concat(...matrix));
+    console.log(max);
 }
 
 biggestElement(
     [
-    [20, 50, 10],
-    [8, 33, 145]
+    [-1, -50, -10],
+    [-8, -33, -145]
 ]);
--- Advanced/Arrays and Nested Arrays/processOddPositions.js
+++ Advanced/Arrays and Nested Arrays/processOddPositions.js
@@ -1,13 +1,18 @@
 function processOddPositions(arr){
-    let newArr = [];
-    arr.reverse();
+//     let newArr = [];
+//     arr.reverse();
     
-    for (el in arr){
-        if (el % 2 == 0){
-            newArr.push(arr[el] * 2);
-        };
-    };
-    return newArr.join(' ');
+//     for (el in arr){
+//         if (el % 2 == 0){
+//             newArr.push(arr[el] * 2);
+//         };
+//     };
+//     return newArr.join(' ');
+// }
+    return arr.filter((a, i) => i % 2 !==0)
+      .map(x => x * 2)
+      .reverse()
+      .join(' ');
 }
 
 processOddPositions([]);
