0360c7ad797845244f5ded0789f0a71173213493
Evgeniy <devil_98@abv.bg>
2022-05-24 13:18:39 +0300

    working with arrays

---
+++ Advanced/Arrays and Nested Arrays/arrays.js
@@ -0,0 +1,9 @@
+// Accessing array elements by index. First and last element of array
+
+let cars = ['BWM', 'Audi', 'Opel'];
+let firstCar = cars[0];
+let lastCar = cars[cars.length -1]
+
+console.log(cars);
+console.log(firstCar)
+console.log(lastCar);
---
+++ Advanced/Arrays and Nested Arrays/equalNeighbors.js
@@ -0,0 +1,14 @@
+function equalNeighbors(arr){
+    let row = 0;
+    let col = 0;
+    let startingElement = arr[row][col];
+    let totalMatch = 0;
+
+
+
+}
+
+equalNeighbors([
+    ['test', 'yes', 'yo', 'ho'],
+    ['well', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', '5']]);
