f22421c69a921d66dc8dd3e66590b233889e0855
Evgeniy <devil_98@abv.bg>
2022-05-25 22:49:25 +0300

    sorting numbers

--- Advanced/Arrays and Nested Arrays/listOfNames.js
+++ Advanced/Arrays and Nested Arrays/listOfNames.js
@@ -1,8 +1,8 @@
 function solve(array){
-    array.sort((a, b) => a.localeCompare(b))
+    array.sort((a, b) => a.localeCompare(b));
     for (index in array){
         console.log(`${Number(index) +1}.${array[index]}`);
-    }
+    };
 }
 
 solve(["John", "Bob", "Christina", "Ema"])
