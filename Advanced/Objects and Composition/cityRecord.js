d46ee340f7f35fa3ebea0963cef49a35239d345a
Evgeniy <devil_98@abv.bg>
2022-05-29 11:09:42 +0300

    city record

---
+++ Advanced/Objects and Composition/cityRecord.js
@@ -0,0 +1,14 @@
+function solve(name, population, treasury){
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+    return city
+
+}
+
+console.log(solve('Santo Domingo',
+12000,
+23500
+));
