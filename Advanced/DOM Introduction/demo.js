61d0c93e8f2680bc2f3c5db9892b0a7bfddf53b9
Evgeniy <devil_98@abv.bg>
2022-06-01 20:18:51 +0300

    DOM demo

---
+++ Advanced/DOM Introduction/demo.js
@@ -0,0 +1,8 @@
+let h1Element = document.getElementsByTagName('h1')[0];
+console.log(h1Element);
+let secondLi = document.getElementsByTagName('li')[1];
+secondLi.innerHTML += " - DONE"
+
+
+let num = Number(element.value);
+element.value = 34
