ba27f5ef3ade1d0e6b3019d01e17b6477b1d5b7b
Evgeniy <devil_98@abv.bg>
2022-06-04 13:22:42 +0300

    ex show more

--- Advanced/DOM Introduction/demo.js
+++ Advanced/DOM Introduction/demo.js
@@ -6,3 +6,11 @@ secondLi.innerHTML += " - DONE";
 // Hide element
 const element = document.getElementById('hidden element');
 element.style.display = 'none';
+
+
+// Match element by n-th Child
+
+const list = document.getElementsByTagName('ul')[0];
+const thirdLi = list.getElementsByTagName('li')[2];
+
+const secondLi = document.querySelector('ul: li:nth-child(2)');
