8d5dff523d3c608f11a764c256f6f45b7412218b
Evgeniy <devil_98@abv.bg>
2022-06-12 19:02:02 +0300

    js for ex

--- Advanced/Advanced Functions/demo.js
+++ Advanced/Advanced Functions/demo.js
@@ -23,4 +23,16 @@ const obje = {
     }
 }
 
-obje.method()
+obje.method();
+
+// currying
+
+function sum3(a) {
+    return (b) => {
+        return (c) => {
+            return a + b + c;
+        }
+    }
+}
+
+console.log(sum3(5)(6)(8));
