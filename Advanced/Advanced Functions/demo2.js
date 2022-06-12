d8f303a0dc0c64aefcde5af0b798ba18fd3951e1
Evgeniy <devil_98@abv.bg>
2022-06-12 18:49:14 +0300

    js for ex

--- Advanced/Advanced Functions/demo2.js
+++ Advanced/Advanced Functions/demo2.js
@@ -13,14 +13,3 @@ const obj = {
 
 obj.printContext();
 
-
-const objs = {
-    name: 'Goshko',
-    outer() {
-        console.log(this)
-        function inner() {console.log(this);}
-        inner()
-    }
-}
-
-objs.outer();
